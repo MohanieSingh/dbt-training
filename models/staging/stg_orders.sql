229cef80b88e6f87b3352b5f90defc33b628bba0
Mohanie Singh <mohanie@consulytic.com>
2023-11-03 13:21:43 +0000

    created a seed called delivery_team.csv and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -20,11 +20,15 @@ select
         p.productname,
         p.subcategory,
     -- calling the markup file which is created in macros by adding arguments
-        {{ markup('ordersellingprice', 'ordercostprice') }} as markup
+        {{ markup('ordersellingprice', 'ordercostprice') }} as markup,
+    -- from delivery team
+        d.delivery_team
 from {{ ref('raw_orders') }} as o
     left join {{ ref('raw_customer') }} as c
         on o.customerid = c.customerid
     left join {{ ref('raw_product') }} as p
         on o.productid = p.productid
+    left join {{ ref('delivery_team') }} as d
+        on o.shipmode = d.shipmode
 -- calling the macro at the end, because it is a where statement
 {{limit_data_in_dev('orderdate')}}
