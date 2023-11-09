d116fb844f94ea7adde7490a706780d2b267801c
Mohanie Singh <mohanie@consulytic.com>
2023-11-09 16:39:04 +0000

    used the source function to refer to the customers table in jaffle_shop

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,9 +1,15 @@
-with customers as 
+with 
+    source as
+    (
+        select * from {{ source('jaffle_shop', 'customers') }}
+    ),
+
+    customers as 
     (
         select  id as customer_id,
                 first_name,
                 last_name
-        from raw.jaffle_shop.customers
+        from source
     )
 
 select * from customers
